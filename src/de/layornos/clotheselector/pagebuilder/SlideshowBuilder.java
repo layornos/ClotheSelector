72c12c8a053ab0bcb18c7957b4af743e0a0c8228
Sandro Koch <layornos@gmail.com>
2017-08-27 17:22:41 +0200

    blub

--- src/de/layornos/clotheselector/pagebuilder/SlideshowBuilder.java
+++ src/de/layornos/clotheselector/pagebuilder/SlideshowBuilder.java
@@ -5,21 +5,31 @@ import java.util.List;
 public class SlideshowBuilder {
 	
 	public static String createSlideshowHTML(List<String> imagePaths) {
-	    String html = "<html><head><meta charset=\"UTF-8\"><title>ClotheSelector - Outfits</title><link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\"></head>\n";
-	    html += "<body>\n"
-	    		+ "<script src=\"/ClotheSelector/jquery.js\"></script>\n"
-	    		+ "<script src=\"/ClotheSelector/trashButton.js\"></script>\n";
-		html += "	<div class=\"w3-container\"><h1>Clothe Selector</h1></div>";	
-	    html += "<div class=\"w3-container\"><h2>Deine Outfits</h2><p>\n";
-	    html += "<div class=\"w3-content w3-display-container\">\n";
+	    String html = "<html><head><meta charset=\"UTF-8\">"+
+	    		"<title>ClotheSelector - Outfits</title>"+
+	    		"<script src=\"/ClotheSelector/jquery.js\"></script>\n" +
+	    		"<script src=\"/ClotheSelector/trashButton.js\"></script>\n" +
+				"<link rel=\"stylesheet\" type=\"text/css\" href=\"all.css\">\n" + 
+				"<title>Clothe Selector</title>\n"+
+	    		"<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">" +
+	    		"<link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\">\n" +
+				"<link rel=\"stylesheet\" href=\"/ClotheSelector/all.css\">\n" +
+				"<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Lato\">\n" + 
+	    		"</head>\n";
+	    html += "<body>\n";
+		html += "<div class=\"w3-container w3-text-teal\"><h1>Clothe Selector</h1></div>";	
+	    html += "<div class=\"w3-container\"><h2>Deine Outfits</h2>\n";
+	    html += "<div class=\"w3-display-container\">\n";
 	    if(imagePaths.isEmpty())
-	    	html += "<h3>Du hast noch keine passenden Outfits hochgeladen!</h3>\n";
+	    	html += "<div class=\"w3-container w3-display-middle\"><h5>Du hast noch <br>keine passenden Outfits <br>hochgeladen!</h5></div>\n";
 	    for(String path : imagePaths) {
 	    	html += "\t<div name=\""+path+"\" class=\"w3-display-container mySlides\">\n\t\t<img src=\""+path+"\" style=\"width:100%\">\n\t\t\t<div class=\"w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black\">"+path+"\n\t\t\t</div>\n\t\t</div>\n";
 	    }
-	    html += "<button class=\"w3-button w3-display-left w3-black\" onclick=\"plusDivs(-1)\">&#10094;</button>\n" + 
-	    		"<button class=\"w3-button w3-display-right w3-black\" onclick=\"plusDivs(1)\">&#10095;</button>\n" + 
-	    		"<button class=\"w3-button w3-display-right w3-blac\" title=\"Delete\" onclick=\"return goto()\">Delete</button>\n" + 
+	    html += "<button class=\"w3-button w3-display-left w3-tear\" onclick=\"plusDivs(-1)\">&#10094;</button>\n" +
+	    		"<button class=\"w3-button w3-display-right w3-tear\" onclick=\"plusDivs(1)\">&#10095;</button>\n" +
+	    		"</div>\n"+
+	    		"<div class=\"w3-display-container\">"+ 
+	    		"<button class=\"w3-button w3-display-right w3-tear\" title=\"Delete\" onclick=\"return goto()\">Delete</button>\n" + 
 	    		"</div>";
 	    
 	    html += "<script>\n" + 
@@ -41,7 +51,7 @@ public class SlideshowBuilder {
 	    		"  x[slideIndex-1].style.display = \"block\";  \n" + 
 	    		"}\n" + 
 	    		"</script>";
-	    html += "</body></html>\n";
+	    html += "</div></body></html>\n";
 		return html;
 	}
 }
--- src/de/layornos/clotheselector/servlets/Index.java
+++ src/de/layornos/clotheselector/servlets/Index.java
@@ -33,25 +33,32 @@ public class Index extends HttpServlet {
 	    		"<script src=\"/ClotheSelector/trashButton.js\"></script>\n" +
 				"<meta charset=\"UTF-8\">\n" +
 				"<link rel=\"stylesheet\" type=\"text/css\" href=\"all.css\">\n" + 
-				"<title>Clothe Selector</title>\n" + 
+				"<title>Clothe Selector</title>\n"+
+				"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" + 
+				"<link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\">\n" +
+				"<link rel=\"stylesheet\" href=\"/ClotheSelector/all.css\">\n" +
+				"<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Lato\">\n" + 
+				"<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">"+ 
 				"</head>\n" + 
 				"<body>\n" + 
-				"<div class=\"w3-container\"><h1>Clothe Selector</h1></div>" +	
+				"<div class=\"w3-container w3-text-teal\"><h1>Clothe Selector</h1></div>" +	
 				"<div class=\"w3-container\"><h2>Search Outfit</h2></div>\n" +
-				"<form action=\"outfits/search/outfit\" method=\"post\"\n" + 
+				"<form class=\"w3-panel\" action=\"outfits/search/outfit\" method=\"post\"\n" + 
 				"		enctype=\"multipart/form-data\">\n" + 
-				"		Season<br> <select name=\"season\">\n" + 
+				"		Season<br> <select class=\"w3-select\" name=\"season\">\n" + 
 				"			<option value=\"SPRING\">Spring</option>\n" + 
 				"			<option value=\"SUMMER\">Summer</option>\n" + 
 				"			<option value=\"AUTUMN\">Autumn</option>\n" + 
 				"			<option value=\"WINTER\">Winter</option>\n" + 
 				"		</select><br> \n" + 
-				"		Occasion<br> <input type=\"text\" id=\"occasion\" name=\"occasion\"><br>\n" + 
-				"		Rain<br> <input type=\"checkbox\" id=\"rain\" name=\"rain\"><br>\n" + 
-				"		<input type=\"submit\" value=\"Search Outfit\" />\n" + 
-				"	</form>\n" +
-				"<button class=\"w3-button w3-display-right w3-blac\" title=\"Add Outfit\" onclick=\"return gotoAddOutfit()\">Add Outfit</button><br><br>\n" +
-				"<button class=\"w3-button w3-display-right w3-blac\" title=\"Show all Outfits\" onclick=\"return gotoAllOutfits()\">Show all Outfits</button>\n" +
+				"		Occasion<br> <input class=\"w3-input\" type=\"text\" id=\"occasion\" name=\"occasion\"><br>\n" + 
+				"		Rain<br> <input class=\"w3-check\" type=\"checkbox\" id=\"rain\" name=\"rain\"><br>\n" + 
+				"</form>\n" +
+				"<div class=\"w3-container\" style=\"width:100%\">\n"+
+				"   \t<button class=\"w3-button w3-display-bottomleft w3-teal\" onclick=\"document.forms[0].submit();\">Search Outfit</button>\n"+
+				"	\t<button class=\"w3-button w3-display-bottommiddle w3-teal\" title=\"Add Outfit\" onclick=\"return gotoAddOutfit()\">Add Outfit</button>\n" +
+				"	\t<button class=\"w3-button w3-display-bottomright w3-teal\" title=\"Show all Outfits\" onclick=\"return gotoAllOutfits()\">Show all Outfits</button>\n" +
+				"</div>\n"+
 				"</body>\n" + 
 				"</html>";
 		response.setContentType("text/html");
